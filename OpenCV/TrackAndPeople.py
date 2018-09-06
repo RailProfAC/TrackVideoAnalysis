2e533ea661531407e2cad5fdbff3eca510e95eb8
Raphael Pfaff <pfaff@fh-aachen.de>
2018-09-06 21:30:50 +0200

    OpenCV works :tada:

---
+++ OpenCV/TrackAndPeople.py
@@ -0,0 +1,61 @@
+# import the necessary packages
+from __future__ import print_function
+from imutils.object_detection import non_max_suppression
+from imutils import paths
+import numpy as np
+import argparse
+import imutils
+import cv2
+
+cap = cv2.VideoCapture('emma.mov')
+size = (640,360)
+print(size)
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+out = cv2.VideoWriter('emmaout.mov',fourcc, 5.0, size)
+
+# initialize the HOG descriptor/person detector
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+while(True):
+    ret, frame = cap.read()
+    #image = imutils.resize(frame, width=min(600, frame.shape[1]))
+    image = imutils.resize(frame, width = 640)   
+    #image = frame 
+    orig = image.copy()
+
+    # detect people in the image
+    (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4),
+        padding=(8, 8), scale=1.05)
+
+    # draw the original bounding boxes
+    for (x, y, w, h) in rects:
+        cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+    # apply non-maxima suppression to the bounding boxes using a
+    # fairly large overlap threshold to try to maintain overlapping
+    # boxes that are still people
+    rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+    pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+
+    # draw the final bounding boxes
+    for (xA, yA, xB, yB) in pick:
+        cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
+
+    # show some information on the number of bounding boxes
+    #filename = imagePath[imagePath.rfind("/") + 1:]
+    #print("[INFO] {}: {} original boxes, {} after suppression".format(
+    #    filename, len(rects), len(pick)))
+    #print('Hier')
+    # show the output images
+    #cv2.imshow("Before NMS", orig)
+    cv2.imshow("After NMS", image)
+    out.write(image)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+out.release()
+cv2.destroyAllWindows()
